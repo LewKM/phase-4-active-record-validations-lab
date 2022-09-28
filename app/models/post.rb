ec2348c0ef429018688e4ae71ce7ced6c2dd8fb5
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-09-28 13:56:14 +0300

    commit tree 1c05fa2b6004356f2cfac8894f9e516c37b54f26

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+
+    # Post Basic validations is expected to validate that :title cannot be empty/falsy
+    validates :title, presence: true
+
+    # Post Basic validations is expected to validate that the length of :content is at least 250
+    validates :content, length: { minimum: 250 }
+
+    # Post Basic validations is expected to validate that the length of :content is at least 250
+    validates :summary, length: { maximum: 250 }
+
+    # Post Basic validations is expected to validate that :category is either ‹"Fiction"› or ‹"Non-Fiction"›
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    # Post Custom validations: title is clickbait is expected not to allow :title to be ‹"True Facts"›
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+        if title && !title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
